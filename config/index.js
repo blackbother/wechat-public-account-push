8bf221e6be5b0cb321dcb278b8be938bdbf87097
blackbother <113665097+blackbother@users.noreply.github.com>
2022-09-19 15:32:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "otZ8y6dFlmPLZk38b3XlPMI8WlhU",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "TWuoYK3Wj4OwPMNsAP5axD3xHjQAHV6Zi9M6EtH1cWg",
+      useTemplateId: "LhKPFdDOyj952crKY8-RGUZ3hH7X0gQ0Hih75QJhOI0",
       // 所在省份
       province: "河北",
       // 所在城市
